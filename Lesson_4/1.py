3f338a3b4f5d3635d0df47bd83619f45c04f343d
Artem Dronkin <adronkin@gmail.com>
2019-12-01 09:57:29 +0300

    HW lesson 4 done

--- Lesson_4/1.py
+++ Lesson_4/1.py
@@ -3,3 +3,131 @@
 разработанных в рамках домашнего задания первых трех уроков.
 Примечание: попробуйте написать несколько реализаций алгоритма и сравнить их.
 """
+
+# 4. Определить, какое число в массиве встречается чаще всего.
+
+from random import randint
+from timeit import Timer
+
+
+def get_count_num(my_list, num):
+    """Возвращает кол-во элементов num в списке"""
+    my_count = 0
+    for i in my_list:
+        if i == num:
+            my_count += 1
+    return my_count
+
+
+def test_1():
+    my_list = []
+    num_count, num = 0, 0
+    
+    while len(my_list) < 500:
+        my_list.append(randint(1, 100))
+    
+    for num in my_list:
+        count_num = get_count_num(my_list, num)
+        if count_num > num_count:
+            num_count = count_num
+            num = num
+    
+    print(f'В списке - {my_list}\n '
+          f'чаще всех встречается "{num}" - {num_count} раз(а).')
+
+
+# t1 = Timer("test_1()", "from __main__ import test_1")
+# print(f'test_1 {t1.timeit(number=1000)} milliseconds')
+
+"""
+=========================================================
+"""
+
+
+def get_count_num(my_list, num):
+    """Возвращает кол-во элементов num в списке"""
+    my_count = 0
+    for i in my_list:
+        if i == num:
+            my_count += 1
+    return my_count
+
+
+def rec_list(my_list):
+    if len(my_list) >= 500:
+        return my_list
+    my_list.append(randint(1, 100))
+    return rec_list(my_list)
+
+
+def test_2():
+    my_list = []
+    num_count, num = 0, 0
+
+    my_list = rec_list(my_list)
+
+    for num in my_list:
+        count_num = get_count_num(my_list, num)
+        if count_num > num_count:
+            num_count = count_num
+            num = num
+
+    print(f'В списке - {my_list}\n '
+          f'чаще всех встречается "{num}" - {num_count} раз(а).')
+
+
+# t2 = Timer("test_2()", "from __main__ import test_2")
+# print(f'test_2 {t2.timeit(number=1000)} milliseconds')
+"""
+=========================================================
+"""
+
+
+def get_count_num(my_list, num):
+    """Возвращает кол-во элементов num в списке"""
+    my_count = 0
+    for i in my_list:
+        if i == num:
+            my_count += 1
+    return my_count
+
+
+def test_3():
+    num_count, num = 0, 0
+
+    my_list = [randint(1, 100) for i in range(500)]
+    set_list = set(my_list)
+
+    for num in set_list:
+        count_num = get_count_num(my_list, num)
+        if count_num > num_count:
+            num_count = count_num
+            num = num
+
+    print(f'В списке - {my_list}\n '
+          f'чаще всех встречается "{num}" - {num_count} раз(а).')
+
+
+# t3 = Timer("test_3()", "from __main__ import test_3")
+# print(f'test_3 {t3.timeit(number=1000)} milliseconds')
+
+"""
+test_1 11.500409675 milliseconds
+test_2 12.557947164 milliseconds
+test_3 3.2712681 milliseconds
+
+test_1
+Реализация алгоритма выполненная в рамках ДЗ к уроку 3. Список генерируется при помощи цикла while.
+Сложность O(n2). Тк необходимо перебрать каждый элемент в списке + повторный перебор для подсчета 
+кол-во вхождений элемента в список.
+
+test_2
+Список генерируется при помощи рекурсии. Скорость выполнения незначительно выросла (на 1 сек).
+Сложность O(n2)
+
+test_3
+Список создается при помощи генератора, что сократило время выполнения примерно на 1 сек (относительно цикла while).
+Добавил переменную в которой сохранил множество созданное из списка, это значительно сократило кол-во элементов 
+для перебора в цикле while. Не придется считать повторно каждого элемента. Врея выполнения сокращено еще на 8 секунд.
+Сложность O(nlogn). Тк для выбора элемента мы используем множество, для подсчета кол-ва вхождений список.
+"""
--- Lesson_4/2.py
+++ Lesson_4/2.py
@@ -3,3 +3,87 @@
 Без использования «Решета Эратосфена»;
 Используя алгоритм «Решето Эратосфена»
 """
+import cProfile
+import timeit
+from math import sqrt
+
+
+MY_NUM = int(input('Введите номер простого числа: '))
+
+
+def test_1(my_num):
+    my_list = []
+    num, num_number = 2, 0
+    while num_number <= my_num:
+        if num > 10:
+            if num % 2 == 0 or num % 5 == 0:
+                pass
+        for i in range(2, num):
+            if i > sqrt(num):
+                my_list.append(num)
+                num_number += 1
+                break
+            if num % i == 0:
+                break
+        else:
+            my_list.append(num)
+            num_number += 1
+        num += 1
+    return my_list[my_num]
+
+
+# print(test_1(MY_NUM - 1))
+
+print(f'test_1 {timeit.timeit("test_1(MY_NUM)", globals=globals(), number=20)} milliseconds')
+cProfile.run('test_1(MY_NUM)')
+
+"""
+=========================================================
+"""
+
+
+def test_2_erat(my_num):
+    my_list = [i for i in range(100000)]
+    my_list[1] = 0
+    for num in my_list:
+        if num > 1:
+            for j in range(num * 2, len(my_list), num):
+                my_list[j] = 0
+    fin_list = [e for e in my_list if e != 0]
+    return fin_list[my_num - 1]
+
+
+# print(test_2_erat(MY_NUM))
+
+print(f'test_2_erat {timeit.timeit("test_2_erat(MY_NUM)", globals=globals(), number=20)} milliseconds')
+cProfile.run('test_2_erat(MY_NUM)')
+
+"""
+1.  Найти простое число под номером 10.
+test_1 0.0010670900000000483 milliseconds
+test_2_erat 0.688657517 milliseconds
+Алгоритм с использованием решета в 688 раз медленнее.
+
+2.  Найти простое число под номером 100.
+test_1 0.017327596999999972 milliseconds
+test_2_erat 0.689248723 milliseconds
+Скорости работы алгоритма с использованием решета осталась без изменений. Скорость вычислений по алгоритму номер 1, 
+увеличилась примерно в 17 раз.
+Алгоритм с использованием решета в 40 раз медленнее.
+
+3.  Найти простое число под номером 1000.
+test_1 0.4456654849999997 milliseconds
+test_2_erat 0.683860095 milliseconds
+Алгоритм с использованием решета в 1.5 раза медленнее.
+
+4.  Найти простое число под номером 9000.
+test_1 10.989988778999999 milliseconds
+test_2_erat 0.6836636089999999 milliseconds
+Скорость работы алгоритма с использованием решета осталась практически без изменений. 
+Скорость вычислений по первому алгоритму значительно возрасла.
+Алгоритм с использованием решета работает в 16 раз быстрее.
+
+Делаем вывод, что алгоритм с использованием решета становится более эффективен при поиске больших значений (от 1000)
+test_1 - алгоритм имеет сложность O(n)
+test_2_erat - имеет сложность O(log n)
+"""

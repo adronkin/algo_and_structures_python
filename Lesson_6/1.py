4441ecc0cd9d35f226cb2f0124f4d19d5d817738
Artem Dronkin <adronkin@gmail.com>
2019-12-09 22:14:39 +0300

    HW 6

--- Lesson_6/1.py
+++ Lesson_6/1.py
@@ -7,3 +7,107 @@
 комментариев к коду. Также укажите в комментариях версию Python
 и разрядность вашей ОС.
 """
+
+# 2. Написать два алгоритма нахождения i-го по счёту простого числа.
+# Без использования «Решета Эратосфена»;
+# Используя алгоритм «Решето Эратосфена»
+
+from math import sqrt
+from memory_profiler import profile
+
+MY_NUM = 5000
+
+
+@profile
+def test_1(my_num):
+    my_list = []
+    num, num_number = 2, 0
+    while num_number <= my_num:
+        if num > 10:
+            if num % 2 == 0 or num % 5 == 0:
+                pass
+        for i in range(2, num):
+            if i > sqrt(num):
+                my_list.append(num)
+                num_number += 1
+                break
+            if num % i == 0:
+                break
+        else:
+            my_list.append(num)
+            num_number += 1
+        num += 1
+    return my_list[my_num - 1]
+
+
+print(test_1(MY_NUM))
+
+"""
+=========================================================
+"""
+
+
+@profile
+def test_2_erat(my_num):
+    my_list = [i for i in range(100000)]
+    my_list[1] = 0
+    for num in my_list:
+        if num > 1:
+            for j in range(num * 2, len(my_list), num):
+                my_list[j] = 0
+    fin_list = [e for e in my_list if e != 0]
+    return fin_list[my_num - 1]
+
+
+print(test_2_erat(MY_NUM))
+
+"""
+Версия Python 3.7.3
+MacOS 10.15
+
+test_1
+Line #    Mem usage    Increment   Line Contents
+================================================
+    22     10.4 MiB     10.4 MiB   @profile
+    23                             def test_1(my_num):
+    24     10.4 MiB      0.0 MiB       my_list = []
+    25     10.4 MiB      0.0 MiB       num, num_number = 2, 0
+    26     10.6 MiB      0.0 MiB       while num_number <= my_num:
+    27     10.6 MiB      0.0 MiB           if num > 10:
+    28     10.6 MiB      0.0 MiB               if num % 2 == 0 or num % 5 == 0:
+    29                                             pass
+    30     10.6 MiB      0.0 MiB           for i in range(2, num):
+    31     10.6 MiB      0.0 MiB               if i > sqrt(num):
+    32     10.6 MiB      0.0 MiB                   my_list.append(num)
+    33     10.6 MiB      0.0 MiB                   num_number += 1
+    34     10.6 MiB      0.0 MiB                   break
+    35     10.6 MiB      0.0 MiB               if num % i == 0:
+    36     10.6 MiB      0.0 MiB                   break
+    37                                     else:
+    38     10.4 MiB      0.0 MiB               my_list.append(num)
+    39     10.4 MiB      0.0 MiB               num_number += 1
+    40     10.6 MiB      0.0 MiB           num += 1
+    41     10.6 MiB      0.0 MiB       return my_list[my_num - 1]
+    
+test_2_erat 
+Line #    Mem usage    Increment   Line Contents
+================================================
+    51     10.6 MiB     10.6 MiB   @profile
+    52                             def test_2_erat(my_num):
+    53     16.6 MiB      0.7 MiB       my_list = [i for i in range(100000)]
+    54     16.6 MiB      0.0 MiB       my_list[1] = 0
+    55     16.6 MiB      0.0 MiB       for num in my_list:
+    56     16.6 MiB      0.0 MiB           if num > 1:
+    57     16.6 MiB      0.0 MiB               for j in range(num * 2, len(my_list), num):
+    58     16.6 MiB      0.0 MiB                   my_list[j] = 0
+    59     11.0 MiB      0.0 MiB       fin_list = [e for e in my_list if e != 0]
+    60     11.0 MiB      0.0 MiB       return fin_list[my_num - 1]
+    
+    
+Первый алгоритм нахождения i-го по счёту простого числа использовал 10.4 MiB памяти.
+Алгоритм с использованием решета Эратосфена использовал 16.6 MiB памяти, т.е. оказался более затратным.
+При нахождении простого числа до 5000 элемента, первый алгоритм значительно более эффективно раходует выделенную память.
+Но при работе с большими числами, учитывая выделенную память и скорость работы алгоритма, 
+алгоритм с использованием решета Эратосфена менее продолжительное время использует выделенную память, 
+а значит более эффективен.
+"""
--- Lesson_6/2.py
+++ Lesson_6/2.py
@@ -5,3 +5,54 @@
 атрибутов. Сравнить, сколько выделяется памяти для хранения атрибутов обоих
 классов.
 """
+
+from pympler import asizeof
+from sys import getsizeof
+
+
+class Person:
+    def __init__(self, name, name2, surname):
+        self.name = name
+        self.name2 = name2
+        self.surname = surname
+
+
+MAN1 = Person('Иван', 'Пертович', 'Каменев')
+print(MAN1.__dict__)
+print(f'asizeof - {asizeof.asizeof(MAN1)}')
+print(f'getsizeof - {getsizeof(MAN1)}')
+MAN1.name_3 = 'Игнатенко'
+print(MAN1.__dict__)
+print(f'asizeof - {asizeof.asizeof(MAN1)}')
+print(f'getsizeof - {getsizeof(MAN1)}')
+
+print('=========================SLOTS=========================')
+
+
+class PersonSlots:
+    __slots__ = ['name', 'name2', 'surname']
+
+    def __init__(self, name, name2, surname):
+        self.name = name
+        self.name2 = name2
+        self.surname = surname
+
+
+MAN2 = PersonSlots('Иван', 'Пертович', 'Каменев')
+print(MAN2.__slots__)
+print(f'asizeof - {asizeof.asizeof(MAN2)}')
+print(f'getsizeof - {getsizeof(MAN2)}')
+
+"""
+=========================СЛОВАРЬ=========================
+{'name': 'Иван', 'name2': 'Пертович', 'surname': 'Каменев'}
+asizeof - 608
+getsizeof - 56
+{'name': 'Иван', 'name2': 'Пертович', 'surname': 'Каменев', 'name_3': 'Игнатенко'}
+asizeof - 760
+getsizeof - 56
+==========================SLOTS==========================
+['name', 'name2', 'surname']
+asizeof - 336
+getsizeof - 64
+"""

3d316d98566c4b4d4546760d9435a20c58a0f13e
Artem Dronkin <adronkin@gmail.com>
2019-12-10 22:30:37 +0300

    HW 7

--- Lesson_7/1.py
+++ Lesson_7/1.py
@@ -4,3 +4,34 @@
 исходный и отсортированный массивы. Сортировка должна быть реализована в
 виде функции. По возможности доработайте алгоритм (сделайте его умнее).
 """
+
+from random import randint
+import timeit
+
+
+def bubble_sorting(list_for_sorting):
+    """
+    Принимает list_for_sorting неотсортированный список.
+    Возвращает отсортированный пузырьковым алгоритмом список.
+    """
+    my_list = list_for_sorting
+    for _ in range(len(my_list)):
+        change = 0
+        for i in range(len(my_list) - 1):
+            if my_list[i] < my_list[i + 1]:
+                my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]
+                change += 1
+        if change == 0:
+            return my_list
+    return my_list
+
+
+UNSORTED_LIST = [randint(-100, 100) for i in range(200)]
+print(f'UNSORTED_LIST:\n{UNSORTED_LIST}\n')
+print(f'SORTED_LIST:\n{bubble_sorting(UNSORTED_LIST)}\n')
+
+print(f'test {timeit.timeit("bubble_sorting(UNSORTED_LIST)", globals=globals(), number=1000)} milliseconds')
+"""
+Стандартная реализация алгоритма - test 5.776569381 milliseconds
+Реализация алгоритма с "change" - test 5.496740742 milliseconds
+"""
--- Lesson_7/2.py
+++ Lesson_7/2.py
@@ -3,3 +3,44 @@
 заданный случайными числами на промежутке [0; 50). Выведите на экран исходный
 и отсортированный массивы.
 """
+
+from random import uniform
+import timeit
+
+
+def merge_sort(list_for_sorting):
+    """
+    Принимает list_for_sorting неотсортированный список.
+    Возвращает отсортированный список алгоритмом сортировка слиянием.
+    """
+    len_list = len(list_for_sorting)
+    if len_list < 2:
+        return list_for_sorting
+
+    left_slice = merge_sort(list_for_sorting[:len_list // 2])
+    right_slice = merge_sort(list_for_sorting[len_list // 2:])
+
+    left, right = 0, 0
+    result = []
+    while left < len(left_slice) or right < len(right_slice):
+        if not left < len(left_slice):
+            result.append(right_slice[right])
+            right += 1
+        elif not right < len(right_slice):
+            result.append(left_slice[left])
+            left += 1
+        elif left_slice[left] < right_slice[right]:
+            result.append(left_slice[left])
+            left += 1
+        else:
+            result.append(right_slice[right])
+            right += 1
+
+    return result
+
+
+UNSORTED_LIST = [uniform(0, 50) for i in range(9)]
+print(f'UNSORTED_LIST:\n{UNSORTED_LIST}\n')
+print(f'SORTED_LIST:\n{merge_sort(UNSORTED_LIST)}\n')
+
+print(f'test {timeit.timeit("merge_sort(UNSORTED_LIST)", globals=globals(), number=1000)} milliseconds')

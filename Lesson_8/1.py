89d0c4e713bd1cc98ec8769c8730ce7d678d6106
Artem Dronkin <adronkin@gmail.com>
2019-12-15 00:23:01 +0300

    HW 8

--- Lesson_8/1.py
+++ Lesson_8/1.py
@@ -1,3 +1,78 @@
 """
 1. Закодируйте любую строку из трех слов по алгоритму Хаффмана.
 """
+
+import heapq
+from collections import Counter
+
+
+class Node:
+    """Узлы дерева"""
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+    def walk(self, code, acc):
+        """В словаре Code добавляем потомкам префиксы 0 и 1"""
+        self.left.walk(code, acc + "0")
+        self.right.walk(code, acc + "1")
+
+
+class Leaf:
+    """Листья дерева"""
+    def __init__(self, char):
+        self.char = char
+
+    def walk(self, code, acc):
+        """Для листа записываем выстроенный код символа в словарь Code"""
+        # '0' если для кодирования введен 1 символ
+        code[self.char] = acc or "0"
+
+
+def huffman_encode(string_for_encode):
+    """Кодирование строки методом Хаффмана.
+    Возращает словарь с символами и соответствующими им кодами"""
+    # Пустой для очереди с приоритетом
+    turn = []
+    # Перебираем ключи и значения (символ и частота) словаря Counter
+    for symb, freq in Counter(string_for_encode).items():
+        # Добавляем  очередь частоту (freq), счетчик, уникальный для
+        # каждого листа (для исключения ValueError в heapq.heappush), лист.
+        turn.append((freq, len(turn), Leaf(symb)))
+    # Превращаем список в очередь с приоритетами
+    heapq.heapify(turn)
+    # Переменная для генирации уникального значения (для исключения ValueError в heapq.heappush)
+    count = len(turn)
+    # Пока в очереди не менее 2х элементов
+    while len(turn) > 1:
+        # Извлекаем из очереди 2 элемента с минимальной частотой
+        freq1, _count1, left = heapq.heappop(turn)
+        freq2, _count2, right = heapq.heappop(turn)
+        # Добавляем в очередь новый внутренний узел с потомками (left и right)
+        heapq.heappush(turn, (freq1 + freq2, count, Node(left, right)))
+        count += 1
+    code = {}
+    # Если в очереди элементов > 0
+    if turn:
+        # Сохраняем в очереди корень дерева
+        [(_freq, _count, root)] = turn
+        # Обходим дерево с корня и заполняем словарь code
+        root.walk(code, "")
+    return code
+
+
+def main():
+    my_string = input("Введите текст для кодирования: ")
+    # Кодируем введенную строку
+    code = huffman_encode(my_string)
+    # Переменная с закодированой строкой
+    encoded = " ".join(code[symb] for symb in my_string)
+    # Печатаем каждый символ в алфавитном порядке и его код
+    for symb in sorted(code):
+        print(f"'{symb}': {code[symb]}")
+    # Печатаем закодированную строку
+    print(encoded)
+
+
+if __name__ == "__main__":
+    main()
--- Lesson_8/2.py
+++ Lesson_8/2.py
@@ -3,3 +3,22 @@
 Пусть дана строка S длиной N, состоящая только из маленьких латинских букв.
 Требуется найти количество различных подстрок в этой строке.
 """
+import hashlib
+
+
+def get_unique_str(my_string, my_set, len_string=1):
+    """Используя хэш-функцию, возвращает сет с уникальными значениями"""
+    if len_string == len(my_string):
+        return my_set
+    for i in range(len(my_string) - 1):
+        temp_string = my_string[i:i + len_string]
+        my_set.add(hashlib.sha1(temp_string.encode('UTF-8')).hexdigest())
+    return get_unique_str(my_string[:len(my_string)], my_set, len_string+1)
+
+
+MY_STRING = input('Введите строку, состоящую только из маленьких латинских букв: ')
+SET_STRINGS = set()
+LEN_STRING = 1
+
+print(f'Кол-во уникальны подстрок в строке "{MY_STRING}" - '
+      f'{len(get_unique_str(MY_STRING, SET_STRINGS))} шт.')
